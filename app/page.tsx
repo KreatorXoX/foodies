93da4c573cc6413eca23ff893d6f2093ace17720
Gorkem Gocer <gorkemgocer7@gmail.com>
2024-08-31 18:34:57 +0300

    Landing page is now ready and responsive, we work on the Community page next

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
+import ImageSlideShow from "@/components/image-slider";
 import PageWrapper from "@/components/page-wrapper";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <PageWrapper>
-      <section className="h-screen  pt-32 pb-20 w-full flex flex-col justify-start gap-10 md:justify-evenly md:flex-row text-md sm:text-lg lg:text-2xl">
-        <div className="w-full ">Image Slider</div>
+      <section className="md:h-screen md:pt-32 pb-20 w-full flex flex-col justify-start gap-10 md:justify-evenly md:flex-row text-md sm:text-lg lg:text-2xl">
+        <div className="w-full flex items-center">
+          <ImageSlideShow />
+        </div>
         <div className=" w-full flex flex-col items-center justify-center gap-7 md:gap-10 lg:gap-20">
           <div className="font-bold tracking-wider text-center md:text-left">
+            <div className="pb-3 md:pb-6 uppercase italic text-orange-400 tracking-widest">
+              <h2 className="text-4xl">Food heads</h2>
+              <h3 className="text-2xl">
+                Make & Share the best foods around the world
+              </h3>
+            </div>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
               delectus voluptatum illo quod, doloribus, at perspiciatis ex
@@ -24,10 +33,10 @@ export default function Home() {
                 About Us
               </Link>
               <Link
-                href={"/community"}
+                href={"/meals"}
                 className="lg:px-4 px-1 py-1 bg-orange-500 rounded-lg hover:bg-orange-700 transition text-center sm:min-w-[8rem] lg:min-w-[12rem]"
               >
-                Our Community
+                Explore Meals
               </Link>
             </div>
             <Link
---
+++ components/image-slider.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+import burgerImg from "@/assets/burger.jpg";
+import curryImg from "@/assets/curry.jpg";
+import dumplingsImg from "@/assets/dumplings.jpg";
+import macncheeseImg from "@/assets/macncheese.jpg";
+import pizzaImg from "@/assets/pizza.jpg";
+import schnitzelImg from "@/assets/schnitzel.jpg";
+import tomatoSaladImg from "@/assets/tomato-salad.jpg";
+
+const images = [
+  { image: burgerImg, alt: "A delicious, juicy burger" },
+  { image: curryImg, alt: "A delicious, spicy curry" },
+  { image: dumplingsImg, alt: "Steamed dumplings" },
+  { image: macncheeseImg, alt: "Mac and cheese" },
+  { image: pizzaImg, alt: "A delicious pizza" },
+  { image: schnitzelImg, alt: "A delicious schnitzel" },
+  { image: tomatoSaladImg, alt: "A delicious tomato salad" },
+];
+
+const ImageSlideShow = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex < images.length - 1 ? prevIndex + 1 : 0
+      );
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="relative overflow-hidden w-full h-96 rounded-lg">
+      <div className="absolute inset-0 flex transition-transform duration-500 ease-in-out">
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            src={image.image}
+            alt={image.alt}
+            className={`absolute inset-0 transition-opacity duration-500 ease-in-out ${
+              index === currentImageIndex ? "opacity-100" : "opacity-0"
+            }`}
+            layout="fill"
+            objectFit="cover"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlideShow;
